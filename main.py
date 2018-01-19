c35bc626760b15a0e4f9371ccede37a1e9122c08
bdaudert <bdaudert@gmail.com>
2018-01-19 13:15:50 -0800

    tempate variable setup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""A simple example of connecting to Earth Engine using App Engine."""
+"""MAIN script for OpenET connecting to Earth Engine using App Engine."""
 
 
 
@@ -25,8 +25,16 @@ import jinja2
 import webapp2
 
 import JinjaFilters
+import templateMethods
 
 # SET STATICS
+'''
+# Load the statics file
+static_dir = config.STATIC_BASE_DIR
+static_file = config.STATIC_FILE
+with open(static_file, 'rb') as fin:
+    statics = json.loads(fin)
+'''
 urlfetch.set_default_fetch_deadline(180000)
 httplib2.Http(timeout=180000)
 
@@ -45,23 +53,120 @@ JINJA_ENVIRONMENT.filters['make_string_range'] = JinjaFilters.make_string_range
 JINJA_ENVIRONMENT.filters['make_int_range'] = JinjaFilters.make_int_range
 JINJA_ENVIRONMENT.filters['divisibleby'] = JinjaFilters.divisibleby
 
-class MainPage(webapp2.RequestHandler):
+#####################################################
+#   RUN APP - RUNS THE APPLICATION AND SETS WEBPAGE
+####################################################
 
-  def get(self):                             # pylint: disable=g-bad-name
-    """Request an image from Earth Engine and render it to a web page."""
+
+def runApp(self, appName, defaultOrNot, method):
+    try:
+        tv = templateMethods.set_initial_template_values(
+            self, appName, method)
+        tv['method'] = method
+    except Exception as e:
+        # This will trigger a hard 500 error
+        # We can't set error and load the default page
+        raise
+    return tv
+
+class defaultApplication(webapp2.RequestHandler):
+    '''
+    defaultApplication, defines:
+        - get and post responses
+        - error handling
+        - logging
+    tv : template_variables
+    '''
     ee.Initialize(config.EE_CREDENTIALS)
-    mapid = ee.Image('srtm90_v4').getMapId({'min': 0, 'max': 1000})
+    ee.data.setDeadline(180000)
 
-    # These could be put directly into template.render, but it
-    # helps make the script more readable to pull them out here, especially
-    # if this is expanded to include more variables.
-    template_values = {
-        'mapid': mapid['mapid'],
-        'token': mapid['token'],
-        'GMAP_API_KEY': config.GMAP_API_KEY
-    }
-    template = JINJA_ENVIRONMENT.get_template('open-et-1.html')
-    self.response.out.write(template.render(template_values))
+    def get(self):
+        if not self.request.arguments():
+            # Initial page load
+            tv = runApp(self, self.appName, 'GET')
+            tv['method'] = 'GET'
+        else:
+            """Loading the main page or a sharelink will trigger a GET"""
+            try:
+                tv = runApp(self, self.appName, 'shareLink')
+                tv['method'] = 'shareLink'
+            except Exception as e:
+                tv = runApp(self, self.appName, 'GET')
+                tv['error'] = str(e)
+                tv['method'] = 'shareLink'
+
+        self.tv_logging(tv, 'GET')
+        template = JINJA_ENVIRONMENT.get_template(self.appHTML)
+        self.response.out.write(template.render(tv))
+
+    def post(self):
+        """Calling Get Map or Get TimeSeries will trigger a POST"""
+        tv = runApp(self, self.appName, 'notDefault', 'POST')
+        if 'method' not in tv.keys():
+            tv['method'] = 'POST'
+        self.tv_logging(tv, 'POST')
+        self.generateResponse(tv)
+
+    def generateResponse(self, tv):
+        """Extract the template values associated with each response variable
+
+        If an error was set in the template_values, generate an error response
+        """
+        dataobj = {}
+        if ('error' in tv.keys() and
+                tv['error']):
+            dataobj['error'] = tv['error']
+            if 'method' in tv.keys():
+                dataobj['method'] = tv['method']
+        else:
+            for tVar in dataStore.responseVars[tv['toolAction']]:
+                try:
+                    dataobj[tVar] = tv[tVar]
+                except KeyError:
+                    # template variable does not exist,
+                    # e.g. timeSeriesTextData2 when only 1 variable,
+                    dataobj[tVar] = []
+        self.response.out.write(json.dumps(dataobj))
+
+    def handle_exception(self, exception, debug):
+        """This catches unhandled Python exceptions in GET requests
+        """
+        logging.exception(exception)
+        appName = self.appName
+        tv = runApp(self, appName, 'default', 'GET')
+        tv['error'] = str(exception)
+        tv['method'] = 'POST'
+        self.generateResponse(tv)
+
+    def tv_logging(self, tv, method='GET'):
+        """Log important template values
+
+        These values are will be written to the appEngine logger
+          so that we can tracks what page requests are being made
+        """
+        tv['method'] = method
+
+        # Skip form values and maxDates
+        log_values = {
+            k: v for k, v in tv.items() if not k.startswith('form')
+        }
+        # Log all values at once
+        logging.info('{}'.format(log_values))
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        """Request an image from Earth Engine and render it to a web page."""
+        ee.Initialize(config.EE_CREDENTIALS)
+        mapid = ee.Image('srtm90_v4').getMapId({'min': 0, 'max': 1000})
+        tv = runApp(self, 'Open-ET-1', 'default', 'pageLoad')
+        tv['GMAP_API_KEY'] = config.GMAP_API_KEY
+        template = JINJA_ENVIRONMENT.get_template('open-et-1.html')
+        self.response.out.write(template.render(tv))
+
+class OpenET(defaultApplication):
+    appName = 'Open-ET-1'
+    appHTML = 'open-et-1.html'
 
 app = webapp2.WSGIApplication([
         ('/', MainPage)
---
+++ mypython/templateMethods.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import statics
+
+
+def set_form_options(variables):
+    form_options = statics.form_options
+    # Override default form options if needed
+    var = variables['variable']
+    # Set field year form option
+    if var == 'fields':
+        years = statics.Polygons.fields.keys()
+        form_options['field_year'] = years
+    # Set datasets
+    form_ds = {}
+    for ds in statics.dataset_by_var[var]:
+    	form_ds[ds] = statics.all_datasets[ds]
+    form_options['dataset'] = form_ds
+    return form_options
+
+
+def set_initial_template_values(RequestHandler, app_name, method):
+    '''
+    Args:
+    RequestHandler: webapp2.RequestHandler object
+    appName: application name, e.g. OpenET-1
+    dOn: default or not; if dOn =  default
+    default values are used
+    otherwisee we try to get the variable
+    values from the request object
+
+    Returns:
+    tv: a dictionary of template variables
+    '''
+    tv = {
+        'app_name': app_name,
+        'variables': statics.variable_defaults,
+        'form_options': {},
+        'map_options': {},
+        'ts_options': {},
+    }
+
+    # Overrode default variables if not GET
+    if method != 'GET':
+        for var_key, dflt in tv['variables'].iteritems():
+            RequestHandler.request.get(var_key, dflt)
+
+    # Set form options
+    tv['form_options'] = set_form_options(tv['variables'])
+    return tv
