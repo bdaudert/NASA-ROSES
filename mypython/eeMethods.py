616ef19afefbf73c12db34a43d20a16b4809dd21
bdaudert <bdaudert@gmail.com>
2018-03-02 09:23:53 -0800

    debug code and get it running for all temp resolutions

--- mypython/eeMethods.py
+++ mypython/eeMethods.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 import logging
 import json
+import os
+import urllib2
 
 import ee
+import cloudstorage as gcs
+from google.appengine.api import app_identity
+
 from config import p_statics
 from config import statics
-
+from config import GEO_BUCKET_NAME
+from config import GEO_BUCKET_URL
 import Utils
+
+
+# Set cloud storage params
+my_default_retry_params = gcs.RetryParams(
+    initial_delay=0.2,
+    max_delay=5.0,
+    backoff_factor=2,
+    max_retry_period=15
+)
+gcs.set_default_retry_params(my_default_retry_params)
+
 
 class ET_Util(object):
     '''
@@ -28,6 +45,24 @@ class ET_Util(object):
         self.et_model = et_model
         self.t_res = t_res  # temporal resolution
         self.missing_value = -9999
+        self.geo_bucket_url = GEO_BUCKET_URL
+
+    def read_data_from_bucket(self):
+        '''
+        # FIX ME: this is not working
+        # path_to_file = '/' + GEO_BUCKET_NAME + '/' + self.geoFName
+        b = os.environ.get(
+            'BUCKET_NAME',
+            app_identity.get_default_gcs_bucket_name())
+        path_to_file = '/' + b + '/' + self.geoFName
+        gcs_file = gcs.open(path_to_file)
+        contents = json.load(gcs_file.read())
+        gcs_file.close()
+        '''
+        f = self.geo_bucket_url + self.geoFName
+        contents = json.load(urllib2.urlopen(f))
+        # print(contents)
+        return contents
 
     def get_collection(self):
         '''
@@ -127,64 +162,78 @@ class ET_Util(object):
             '''
             Averages the ee.Image over all pixels of ee.Geometry
             '''
-            try:
-                reduced_image_data = img.reduceRegion(
-                    ee.Reducer.mean(),
-                    geometry=geom,
-                    scale=1000,
-                    tileScale=1,
-                    crs='EPSG:4326',
-                    crsTransform=None,
-                    bestEffort=True
-                )
-                val = reduced_image_data.get(variable)
-            except:
-                val = self.missing_value
-            return ee.Feature(geom, {'Data': val})
-
-
+            reduced_image_data = img.reduceRegion(
+                ee.Reducer.mean(),
+                geometry=geom,
+                scale=1000,
+                tileScale=1,
+                crs='EPSG:4326',
+                crsTransform=None,
+                bestEffort=True
+            )
+            return ee.Feature(None, reduced_image_data)
 
         et_data = {}
         imgs = []
-        count = 0
-        stat_names_list = []
         # FIX ME: add more vars as data comes online
-        # for v in var_list:
         # MODIS SSEBop only has et right now
-        # var_list = statics['cols_by_var_res'].keys()
-        for v in ['et']:
+        # var_list = statics['stats_by_var_res'].keys()
+        var_list = ['et']
+        for v in var_list:
+            # Filter collection by variable
+            c_var = self.filter_coll_by_var(coll, v)
             logging.info('PROCESSING VARIABLE ' + str(v))
-            stat_names = statics['cols_by_var_res'][v][self.t_res]
+            stat_names = statics['stats_by_var_res'][v][self.t_res]
             for stat_name in stat_names:
-                stat_names_list.append(stat_name)
                 logging.info('PROCESSING STATISTIC ' + str(stat_name))
-                variable = stat_name.split('_')[0].lower()
+                res = stat_name.split('_')[1]
+                # Filer collection by date
+                dS_str = str(self.year) + '-' +\
+                    p_statics['start_end_mon_days_by_res'][res][0]
+                dE_str = str(self.year) + '-' +\
+                    p_statics['start_end_mon_days_by_res'][res][1]
+                c_t = self.filter_coll_by_dates(c_var, dS_str, dE_str)
                 temporal_stat = p_statics['t_stat_by_var']
-                # coll = self.filter_coll_by_var(coll, variable)
-                imgs.append(self.reduce_collection_to_img(coll, temporal_stat))
-                count += 1
+                img = self.reduce_collection_to_img(c_t, temporal_stat)
+                # feats = ee.FeatureCollection(average_over_region(img))
+                imgs.append(img)
+            ee_imgs = ee.ImageCollection(imgs)
+            feats = ee.FeatureCollection(ee_imgs.map(average_over_region))
 
-        ee_imgs = ee.ImageCollection(imgs)
-        feats = ee.FeatureCollection(ee_imgs.map(average_over_region))
-        try:
-            f_data = feats.aggregate_array('Data').getInfo()
-        except Exception as e:
-            raise Exception(e)
+            try:
+                f_data = feats.getInfo()
+            except Exception as e:
+                f_data = {'features': []}
+                logging.error(e)
 
-        for c_idx in range(count):
-            stat_name = stat_names_list[c_idx]
-            et_data[stat_name] = f_data
+            for stat_idx, stat in enumerate(stat_names):
+                if 'features' not in f_data.keys() or not f_data['features']:
+                    et_data[stat] = -9999
+                    continue
+                try:
+                    feat = f_data['features'][stat_idx]
+                except:
+                    et_data[stat] = -9999
+                    continue
+
+                if 'properties' not in feat.keys():
+                    et_data[stat] = -9999
+                    continue
+                try:
+                    val = feat['properties'][v]
+                    et_data[stat] = round(val, 4)
+                except:
+                    et_data[stat] = -9999
+                    continue
         return et_data
 
     def get_et_stats(self):
         coll = self.get_collection()
-        dS_str = str(self.year) + '-' +\
-            p_statics['start_end_mon_days_by_res'][self.t_res][0]
-        dE_str = str(self.year) + '-' +\
-            p_statics['start_end_mon_days_by_res'][self.t_res][1]
-        coll = self.filter_coll_by_dates(coll, dS_str, dE_str)
+        geo_data = self.read_data_from_bucket()
+        '''
         with open(self.geoFName, 'r') as geo_infile:
             geo_data = json.load(geo_infile)
+        '''
         json_data = {
             'type': 'FeatureCollection',
             'features': []
