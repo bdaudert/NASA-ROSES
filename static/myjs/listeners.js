c2651110aed2248b80f5880347bc14de7286bfe1
bdaudert <bdaudert@gmail.com>
2018-05-03 09:55:07 -0700

    code cleanup in prep for multi year summaries

--- static/myjs/listeners.js
+++ static/myjs/listeners.js
@@ -5,45 +5,6 @@ $(document).ready(function () {
     $('.modal-content').resizable();
     $('.modal-dialog').draggable();
 
-    //-------------------------
-    // Date Picker
-    //-------------------------
-    /*
-    $( "#start_date" ).datepicker({
-        showOn: "button",
-        buttonImage: "media/img/calendar_logo.gif",
-        buttonImageOnly: true,
-        buttonText: "Open Calendar",
-        changeMonth: true,
-        changeYear: true,
-        numberOfMonths: 3,
-        minDate: $('#min_date').val(),
-        maxDate: $('#max_date').val(),
-        yearRange: $('#min_date').val().slice(0,4) + ":" + $('#max_date').val().slice(0,4),
-        dateFormat: "yy-mm-dd",
-        onClose: function( selectedDate ) {
-            $( "#end_date" ).datepicker( "option", "minDate", selectedDate);
-        }
-    });
-
-    $( "#end_date" ).datepicker({
-        showOn: "button",
-        buttonImage: "media/img/calendar_logo.gif",
-        buttonImageOnly: true,
-        buttonText: "Open Calendar",
-        changeMonth: true,
-        changeYear: true,
-        numberOfMonths: 3,
-        minDate: $('#min_date').val(),
-        maxDate: $('#max_date').val(),
-        yearRange: $('#min_date').val().slice(0,4) + ":" + $('#max_date').val().slice(0,4),
-        dateFormat: "yy-mm-dd",
-        onClose: function( selectedDate ) {
-            $( "#start_date" ).datepicker( "option", "maxDate", selectedDate);
-        }
-    });
-    */
-    
     $('#region').on('change', function(){
         change_inRegion($(this).val());
 
@@ -52,13 +13,16 @@ $(document).ready(function () {
     $('#field_year').on('change', function(){
         change_inYear($(this).val());
     });
+
     $('#variable').on('change', function(){
         change_inVariable($(this).val());
     });
+
     $('#t_res').on('change', function(){
         change_inTRes($(this).val());
     });
-     $('#time_period').on('change', function(){
+
+    $('#time_period').on('change', function(){
         change_inTimePeriod($(this).val());
     });
 
