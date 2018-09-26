5f0cde16c6e631fcbdcd5c76a11d627762ff679f
bdaudert <bdaudert@gmail.com>
2018-09-26 11:30:43 -0700

    clean up main code

--- main.py
+++ main.py
@@ -25,30 +25,12 @@ from mypython import templateMethods
 from mypython import databaseMethods
 from mypython import JinjaFilters
 
-
-
-
-
 # SET STATICS
 httplib2.Http(timeout=180000)
 
-'''
-# Set the JINJA_ENVIRONMENT
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-JINJA_ENVIRONMENT = jinja2.Environment(
-    autoescape=True,
-    loader=jinja2.FileSystemLoader(template_dir),
-    extensions=['jinja2.ext.with_'])
-
-# Register custom filters
-JINJA_ENVIRONMENT.filters['is_in'] = JinjaFilters.is_in
-JINJA_ENVIRONMENT.filters['not_in'] = JinjaFilters.not_in
-JINJA_ENVIRONMENT.filters['make_string_range'] = JinjaFilters.make_string_range
-JINJA_ENVIRONMENT.filters['make_int_range'] = JinjaFilters.make_int_range
-JINJA_ENVIRONMENT.filters['divisibleby'] = JinjaFilters.divisibleby
-'''
-
+# Set the flask app
 app = flask.Flask(__name__)
+
 # Set the template filters
 from inspect import getmembers, isfunction
 template_filters = {name: function
@@ -112,68 +94,7 @@ def home():
                     response[var] = []
         return json.dumps(response, ensure_ascii=False)
 
-        #return json.dumps(tv, ensure_ascii=False)
 
-    def handle_exception(exception, debug):
-        """This catches unhandled Python exceptions in GET requests
-        """
-        logging.exception(exception)
-        tv = runApp(None, app_name, 'GET')
-        tv['error'] = str(exception)
-        # override method
-        tv['method'] = 'POST'
-        return flask.render_template('nasa-roses.html', **tv)
-
-    def tv_logging(tv, method):
-        """Log important template values
-        These values are will be written to the appEngine logger
-          so that we can tracks what page requests are being made
-        """
-        # Skip form values and maxDates
-        log_values = {
-            k: v for k, v in tv.items()
-            if not k.startswith('form') and
-            not k.startswith('etdata') and
-            not k.startswith('metadata') and
-            not k.startswith('geomdata') and
-            not k.startswith('featsgeomdata') and
-            not k.startswith('featsdata')
-        }
-        # Log all values at once
-        logging.info('{}'.format(log_values))
-
-
-'''
-# FIX ME: we need to replace the users module withsomething that
-# is supported with Python 3.7
-from google.appengine.api import users
-class AdminPage(webapp2.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if user:
-            if users.is_current_user_admin():
-                self.response.write('You are an administrator.')
-            else:
-                self.response.write('You are not an administrator.')
-        else:
-            self.response.write('You are not logged in.')
-
-class LogInPage(webapp2.RequestHandler):
-    def get(self):
-        # [START user_details]
-        user = users.get_current_user()
-        if user:
-            nickname = user.nickname()
-            logout_url = users.create_logout_url('/')
-            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
-                nickname, logout_url)
-        else:
-            login_url = users.create_login_url('/')
-            greeting = '<a href="{}">Sign in</a>'.format(login_url)
-        # [END user_details]
-        self.response.write(
-            '<html><body>{}</body></html>'.format(greeting))
-'''
 @app.route('/databaseTasks', methods=['GET'])
 def databaseTasks():
     app_name = 'databaseTask'
@@ -192,6 +113,20 @@ def databaseTasks():
                     DU.add_to_db()
             logging.info(region + '/' + year + ' PROCESSED!')
     return flask.render_template('databaseTasks.html', **tv)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    logging.exception('A 404 error occurred during a request.')
+    return 'Sorry, Nothing at this URL.', 404
+
+
+@app.errorhandler(500)
+def application_error(e):
+    """Return a custom 500 error."""
+    logging.exception('A 500 error occurred during a request.')
+    return 'Sorry, unexpected error: {}'.format(e), 500
+
 
 
 if __name__ == '__main__':
