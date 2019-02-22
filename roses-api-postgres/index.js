4d35905872626ecea6909fcac3f218f3f75ba56b
bdaudert <bdaudert@gmail.com>
2019-02-21 16:22:33 -0800

    created config file with login info

--- roses-api-postgres/index.js
+++ roses-api-postgres/index.js
@@ -16,18 +16,16 @@ app.use(
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-
-
-app.get('/map_geojson/:user_id:feature_collection_name:start_date:end_date:model:variable:temporal_resoution', db.getMapGeojson)
-
-/*
+app.get('/test/:feature_collection_name', db.test)
+app.get('/map_geojson/:user_id/:feature_collection_name/:start_date/:end_date/:model/:variable/:temporal_resoution', db.get_getMapGeojson)
+app.post('/map_geojson', db.post_getMapGeojson)
 app.get('/etdata', db.getEtdata)
 app.get('/etdata/:id', db.getEtdataById)
 app.post('/etdata', db.postEtdata)
 app.post('/etdatarange', db.postEtdataRange)
-*/
+
 
 app.listen(port, () => {
-  console.log('App running on port ${port}.')
+  console.log('App running on port ' + String(port));
 })
 
--- roses-api-postgres/queries.js
+++ roses-api-postgres/queries.js
@@ -1,15 +1,38 @@
+
 const Pool = require('pg').Pool
 // Credentials for test database, remove from source control for production API!!!!
+const config = require('./config.json')
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'test',
-  password: '',
-  port: 5432,
+  user: config.postgis_user,
+  host: config.postgis_host,
+  database: config.postgis_database,
+  password: config.postgis_password,
+  port: config.postgis_port,
 })
+/*
+const pool = new Pool({
+  user: config.vTile_user,
+  host: config.vTile_host,
+  database: config.vTile_database,
+  password: config.vTile_password,
+  port: config.vTile_port,
+})
+*/
 
+const test = (request, response) => {
+    const fc = String(request.params.feature_collection_name);
+    console.log(fc);
+    pool.query('SELECT COUNT(roses.feature.feature_id) FROM roses.feature WHERE roses.feature.feature_collection_name = $1', [fc],
+        (error, results) => {
+          if (error) {
+            throw error;
+          }
+          response.status(200).json(results.rows)
+      })
+}
 
-const getMapGeojson = (request, response) => {
+const get_getMapGeojson = (request, response) => {
      const uid = parseInt(request.params.user_id);
      const fc = String(request.params.feature_collection_name);
      const sd = String(request.params.start_date);
@@ -19,30 +42,28 @@ const getMapGeojson = (request, response) => {
      const tr = String(request.params.temporal_resolution);
 
      pool.query(
-       'SELECT\n' +
-       '        ST_AsGeoJSON(roses.feature.geometry) AS geom,\n' +
-       '        roses.feature.feature_id AS feat_id,\n' +
-       '        json_agg(roses.feature_metadata.feature_metadata_name) AS meta_name,\n' +
-       '        json_agg(roses.feature_metadata.feature_metadata_name) AS meta_value,\n' +
-       '        roses.timeseries.start_date AS sd,\n' +
-       '        roses.timeseries.end_date AS ed,\n' +
-       '        roses.timeseries.data_value AS dv\n' +
-       '        \n' +
-       '        FROM\n' +
-       '        roses.timeseries\n' +
-       '        LEFT JOIN roses.data ON roses.data.timeseries_id = roses.timeseries.timeseries_id\n' +
-       '        LEFT JOIN roses.feature ON roses.feature.feature_id = roses.data.feature_id\n' +
-       '        LEFT JOIN roses.feture_metadata ON roses.feature_metadata.feature_id = roses.data.feature_id\n' +
-       '        \n' +
-       '        WHERE\n' +
-       '        roses.feature.feature_collection_name = $1\n' +
-       '        AND roses.timeseries.start_date >= $2::timestamp\n' +
-       '        AND roses.timeseries.end_date <= $2::timestamp\n' +
-       '        AND roses.data.user_id = $4\n' +
-       '        AND roses.data.model_name = $5\n' +
-       '        AND roses.data.variable_name = $6\n' +
-       '        AND roses.data.temporal_resolution = $7\n' +
-       '        ORDER BY roses.feature.feature_id', [fc, sd, ed, uid, m, v, tr],
+       `SELECT 
+       ST_AsGeoJSON(roses.feature.geometry) AS geom, 
+       roses.feature.feature_id AS feat_id,
+       json_agg(roses.feature_metadata.feature_metadata_name) AS meta_name,
+       json_agg(roses.feature_metadata.feature_metadata_name) AS meta_value,
+       roses.timeseries.start_date AS sd,
+       roses.timeseries.end_date AS ed,
+       roses.timeseries.data_value AS dv
+       FROM
+       roses.timeseries
+       LEFT JOIN roses.data ON roses.data.timeseries_id = roses.timeseries.timeseries_id
+       LEFT JOIN roses.feature ON roses.feature.feature_id = roses.data.feature_id
+       LEFT JOIN roses.feture_metadata ON roses.feature_metadata.feature_id = roses.data.feature_id
+       WHERE
+       roses.feature.feature_collection_name = $1
+       AND roses.timeseries.start_date >= $2::timestamp
+       AND roses.timeseries.end_date <= $2::timestamp
+       AND roses.data.user_id = $4
+       AND roses.data.model_name = $5
+       AND roses.data.variable_name = $6
+       AND roses.data.temporal_resolution = $7
+       ORDER BY roses.feature.feature_id`, [fc, sd, ed, uid, m, v, tr],
        (error, results) => {
           if (error) {
             throw error;
@@ -50,8 +71,40 @@ const getMapGeojson = (request, response) => {
           response.status(200).json(results.rows)
       })
 }
+const post_getMapGeojson = (request, response) => {
+    const { fc, sd, ed, uid, m, v, tr } = request.body
+    pool.query(
+       'SELECT' +
+       ' ST_AsGeoJSON(roses.feature.geometry) AS geom,' +
+       ' roses.feature.feature_id AS feat_id,' +
+       ' json_agg(roses.feature_metadata.feature_metadata_name) AS meta_name,' +
+       ' json_agg(roses.feature_metadata.feature_metadata_name) AS meta_value,' +
+       ' roses.timeseries.start_date AS sd,' +
+       ' roses.timeseries.end_date AS ed,' +
+       ' roses.timeseries.data_value AS dv' +
+       ' FROM' +
+       ' roses.timeseries' +
+       ' LEFT JOIN roses.data ON roses.data.timeseries_id = roses.timeseries.timeseries_id' +
+       ' LEFT JOIN roses.feature ON roses.feature.feature_id = roses.data.feature_id' +
+       ' LEFT JOIN roses.feture_metadata ON roses.feature_metadata.feature_id = roses.data.feature_id' +
+       ' WHERE' +
+       ' roses.feature.feature_collection_name = $1' +
+       ' AND roses.timeseries.start_date >= $2::timestamp' +
+       ' AND roses.timeseries.end_date <= $2::timestamp' +
+       ' AND roses.data.user_id = $4' +
+       ' AND roses.data.model_name = $5' +
+       ' AND roses.data.variable_name = $6' +
+       ' AND roses.data.temporal_resolution = $7' +
+       ' ORDER BY roses.feature.feature_id', [fc, sd, ed, uid, m, v, tr],
+       (error, results) => {
+          if (error) {
+            throw error;
+          }
+          response.status(201).json(results.rows)
+    })
+}
 
-/*
+
 const getEtdata = (request, response) => {
   pool.query('SELECT ogc_fid, cmdtymast, acresmast, et_2017 FROM base15_ca_poly_170616_data_all ORDER BY ogc_fid ASC', (error, results) => {
     if (error) {
@@ -99,5 +152,7 @@ module.exports = {
   getEtdataById,
   postEtdata,
   postEtdataRange,
+  test,
+  get_getMapGeojson,
+  post_getMapGeojson
 }
-*/
